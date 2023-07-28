ce331e732b84f092406961b1c9c4fddc069bbba2
Keenan Chiasson <Keenan.Chiasson@ACSTexas.com>
2023-07-28 13:48:04 -0500

    Initial Commit

---
+++ DocuSignPoC/DocuSignPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocuSignPoCApp.swift
+//  DocuSignPoC
+//
+//  Created by Keenan Chiasson on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DocuSignPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
